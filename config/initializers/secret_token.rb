3697df6c4b3d92b9356e9722c42cbe1bf1bd01ad
Mark Dickie <markd@mavennetwork.co.uk>
2014-01-04 20:50:01 +0000

    Initial commit with only rails and bootstrap set up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mavencc::Application.config.secret_key_base = 'a61037074c6fdaf705ba34bfa6d7cc716a230dffbce87232f2075ad7fb19ca7094196aaac5197bf300245395c35ec3edb83dbc52512b81a012227597e40b8cf0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mavencc::Application.config.session_store :cookie_store, key: '_mavencc_session'
