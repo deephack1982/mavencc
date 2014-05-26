c3b47ed9e9df38b7bb2c0f89f85cd992f2b6a6bf
Mark Dickie <markd@mavennetwork.co.uk>
2014-05-26 15:15:54 +0100

    Basic implementation of campaigns with CRUD

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,11 +1,59 @@
 class CampaignsController < ApplicationController
 	helper_method :sort_column, :sort_direction
 	def index
-		@campaign_tab = 'list'
 		@campaigns = Campaign.order(sort_column + " " + sort_direction).paginate(page: params[:page], :per_page => 20)
+		@campaign_tab = 'list'
+	end
+	
+	def show
+		@campaign = Campaign.find(params[:id])
+		@campaign_tab = 'list'
+	end
+	
+	def new
+		@campaign = Campaign.new
+		@campaign_tab = 'add'
+	end
+	
+	def create
+		@campaign = Campaign.create(campaign_params)
+    	if @campaign.save
+    		flash[:success] = "Campaign created"
+      		redirect_to campaign_path(@campaign)
+    	else
+    		flash[:danger] = "Campaign could not be created"
+      		render 'new'
+    	end
+	end
+	
+	def edit
+		@campaign = Campaign.find(params[:id])
+		@campaign_tab = 'list'
+	end
+	
+	def update
+		@campaign = Campaign.find(params[:id])
+		if @campaign.update_attributes(campaign_params)
+			flash[:success] = "Campaign updated"
+			@campaign.campaign_id = @campaign.id
+			redirect_to campaign_path(@campaign)
+		else
+			flash[:danger] = "Campaign could not be updated"
+			render 'edit'
+		end
+	end
+	
+	def destroy
+		Campaign.find(params[:id]).destroy
+		flash[:success] = "Campaign Deleted"
+		redirect_to campaigns_path
 	end
 	
 	private
+	
+	def campaign_params
+		params.require(:campaign).permit(:campaign_id,:campaign_name,:active,:user_group,:campaign_description)
+	end
 	
 	def sort_column
 		Campaign.column_names.include?(params[:sort]) ? params[:sort] : "campaign_id"
