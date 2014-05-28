b9ccdffb896c9a139c111293faa57f87f580691f
Mark Dickie <markd@mavennetwork.co.uk>
2014-05-28 21:12:43 +0100

    Bumped completion bar on home page to 25%

--- db/schema.rb
+++ db/schema.rb
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 20140527111722) do
     t.string   "user"
     t.string   "vendor_lead_code"
     t.string   "source_id"
-    t.integer  "lists_id"
+    t.integer  "list_id"
     t.string   "called_since_last_reset"
     t.string   "phone_code"
     t.string   "phone_number"
@@ -61,7 +61,7 @@ ActiveRecord::Schema.define(version: 20140527111722) do
     t.datetime "updated_at"
   end
 
-  add_index "list", ["lists_id"], name: "index_list_on_lists_id", using: :btree
+  add_index "list", ["list_id"], name: "index_list_on_list_id", using: :btree
 
   create_table "lists", force: true do |t|
     t.string   "list_id"
