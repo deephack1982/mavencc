49c359aede1169eec76bcccc61c5c391cb638e54
Mark Dickie <markd@mavennetwork.co.uk>
2014-06-23 15:12:54 +0100

    Corrected user and user group db seed fixes #7

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Site.create!(company: 'AREUK', site: 'Darlington', license_type: '20', license_code: 'testtesttest', modules: { 'AMD' => true, 'Recording' => true, 'CRM Integration' => false })
-User.create!(user: 6666, pass: 'pepper1899', full_name: 'Administrator', user_level: 9, user_group: 'ADMIN')
+UserGroup.create!(user_group: 'Admins', group_name: 'Administrative Group')
+UserGroup.create!(user_group: 'Operators', group_name: 'Operators group')
+User.create!(user: 6666, pass: 'pepper1899', full_name: 'Administrator', user_level: 9, user_group: 'Admin')
 CSV.foreach("#{::Rails.root}/lib/data/statuses.csv") do |row|
 	Status.create!(status: row[0], status_name: row[1], selectable: row[2], human_answered: row[3], category: row[4], sale: row[5], dnc: row[6], customer_contact: row[7], not_interested: row[8], unworkable: row[9], scheduled_callback: row[10], completed: row[11])
 end
