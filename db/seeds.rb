5ee49b7c7c731417967cb67b53268c3a60bfee8a
Mark Dickie <markd@mavennetwork.co.uk>
2014-07-03 13:37:52 +0100

    Forget previous commit, this one adds the campaign seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,7 @@ Site.create!(company: 'AREUK', site: 'Darlington', license_type: '20', license_c
 UserGroup.create!(user_group: 'Admins', group_name: 'Administrative Group')
 UserGroup.create!(user_group: 'Operators', group_name: 'Operators group')
 User.create!(user: 6666, pass: 'pepper1899', full_name: 'Administrator', user_level: 9, user_group: 'Admin')
+Campaign.create!(campaign_id: 'Test', campaign_name: 'Test Campaign', active = 'N')
 CSV.foreach("#{::Rails.root}/lib/data/statuses.csv") do |row|
 	Status.create!(status: row[0], status_name: row[1], selectable: row[2], human_answered: row[3], category: row[4], sale: row[5], dnc: row[6], customer_contact: row[7], not_interested: row[8], unworkable: row[9], scheduled_callback: row[10], completed: row[11])
 end
