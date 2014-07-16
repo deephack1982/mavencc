dbe818a599c4aae607a60b92111ca13ca2f3bfb0
Mark Dickie <markd@mavennetwork.co.uk>
2014-07-16 17:19:20 +0100

    Added sorting to system statuses, fixes #34

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,14 +1,25 @@
 class StatusesController < ApplicationController
+	helper_method :sort_column, :sort_direction
 
 	before_filter :authorise
 
 	def index
 		@admin_tab = 'status'
-		@statuses = Status.all
+		@statuses = Status.order(sort_column + " " + sort_direction)
 	end
 	
 	def show
 		@admin_tab = 'status'
 		@status = Status.find(params[:id])
 	end
+	
+	private
+	
+		def sort_column
+			Status.column_names.include?(params[:sort]) ? params[:sort] : "id"
+		end
+	
+		def sort_direction
+			%w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+		end
 end
