2998be887cf235dcbfe4f561d4996c45f5ef6cad
Mark Dickie <markd@mavennetwork.co.uk>
2014-07-21 17:26:56 +0100

    Added user level checks, refs #41

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,15 @@ class SessionsController < ApplicationController
 
   def create
 		user = User.find_by_user(params[:user])
-		if user && user.authenticate(params[:password]) && user.active == 'Y'
+		if user && user.authenticate(params[:password]) && user.active == 'Y' && user.user_level == 9
 			session[:user_id] = user.id
 			redirect_to root_url flash[:success] = "Logged In"
+		elsif user && user.active == 'N'
+			redirect_to login_path flash[:danger] = "User not active"
+		elsif user && user.user_level < 9
+			redirect_to login_path flash[:danger] = "User not admin"
 		else
-			flash[:danger] = "Incorrect Log In"
+			redirect_to login_path flash[:danger] = "Incorrect Log In"
 		end
   end
   
