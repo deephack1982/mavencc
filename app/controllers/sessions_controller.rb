d9f6050b11e7fd8b876c4c66454452adb0060408
Mark Dickie <markd@mavennetwork.co.uk>
2014-07-21 17:20:39 +0100

    Added active yes check to login, fixes #41

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
 		user = User.find_by_user(params[:user])
-		if user && user.authenticate(params[:password])
+		if user && user.authenticate(params[:password]) && user.active == 'Y'
 			session[:user_id] = user.id
 			redirect_to root_url flash[:success] = "Logged In"
 		else
