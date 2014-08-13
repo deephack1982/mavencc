abd53cc2af086b584ed128769e49404a8a77574c
Mark Dickie <markd@mavennetwork.co.uk>
2014-08-13 22:11:07 +0100

    Fixed incorrect paths in authorise for users and user_groups

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,11 +12,11 @@ class ApplicationController < ActionController::Base
 		redirect_to login_url if current_user.nil?
 		if current_user.delete_users == '0' && params[:controller] == 'users' && params[:action] == 'destroy'
 			flash[:danger] = 'Insufficient Permissions'
-			redirect_to campaigns_path
+			redirect_to users_path
 		end
 		if current_user.delete_user_groups == '0' && params[:controller] == 'user_groups' && params[:action] == 'destroy'
 			flash[:danger] = 'Insufficient Permissions'
-			redirect_to campaigns_path
+			redirect_to user_groups_path
 		end
 		if current_user.download_lists == '0' && params[:controller] == 'lists' && params[:action] == 'show.csv'
 			flash[:danger] = 'Insufficient Permissions'
